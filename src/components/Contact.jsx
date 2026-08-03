8ba59fad83df7006ef36ddb7066499e4e9f079d3
Joe Blake <jhblake@outlook.com>
2026-08-03 18:56:07 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,25 +9,7 @@ export default function Contact() {
         <p className="contact__subtext">
           Tell us a bit about what you're working on and we'll follow up to schedule an
           introductory call.
-        </p>
-        <p><!--
-    PeoplePerHour Profile Widget
-    The div#pph-hire me is the element
-    where the iframe will be inserted.
-    You may move this element wherever
-    you need to display the widget
--->
-<div id="pph-hireme"></div>
-<script type="text/javascript">
-(function(d, s) {
-    var useSSL = 'https:' == document.location.protocol;
-    var js, where = d.getElementsByTagName(s)[0],
-    js = d.createElement(s);
-    js.src = (useSSL ? 'https:' : 'http:') +  '//www.peopleperhour.com/hire/2430130562/13508338.js?width=245&height=320&orientation=vertical&theme=light&rnd='+parseInt(Math.random()*10000, 10);
-    try { where.parentNode.insertBefore(js, where); } catch (e) { if (typeof console !== 'undefined' && console.log && e.stack) { console.log(e.stack); } }
-}(document, 'script'));
-</script></p>
-        
+        </p> 
         <a className="button button--primary" href={`mailto:${CONTACT_EMAIL}`}>
           {CONTACT_EMAIL}
         </a>
